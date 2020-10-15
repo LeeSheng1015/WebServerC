e15eec2a10b93ba0bb99c6bc809109d986610486
Lee <62130355+LeeSheng1015@users.noreply.github.com>
2020-10-15 18:17:26 +0800

    Add files via upload

---
+++ noncopyable.h
@@ -0,0 +1,13 @@
+// @Author Li Sheng
+// @Email 727151354@qq.com
+#pragma once
+
+class noncopyable {
+ protected:
+  noncopyable() {}
+  ~noncopyable() {}
+
+ private:
+  noncopyable(const noncopyable&);
+  const noncopyable& operator=(const noncopyable&);
+};
